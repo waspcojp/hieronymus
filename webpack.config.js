925f37ce1f63dccbb02b816e68e0f26c594a2cca
Masami Ogoshi <ogochan@wasp.co.jp>
2022-08-20 11:09:58 +0000

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const MODE = "development";
+//const MODE = "production";
+
+const prod = MODE === 'production';
+
+module.exports = {
+	mode: MODE,
+	context: path.resolve(__dirname, 'front/javascripts'),
+    entry: {
+		login: './login.js',
+		home: './home.js',
+		common: './common.js',
+		journal: './journal.js',
+		ledger: './ledger.js',
+		accounts: './accounts.js',
+		trial_balance: './trial-balance.js',
+		customer: './customer.js',
+		voucher: './voucher.js',
+		'bank-ledger': './bank-ledger.js'
+	},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './dist/',
+        filename: '[name].js'
+    },
+    devtool: false,
+	watchOptions: {
+		ignored: [ 'node_modules', 'public', 'views', 'tests', 'temp', 'models', 'migrations', 'dist', 'config', 'routes' ]
+	},
+	module: {
+        rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						compilerOptions: {
+							dev: !prod
+						},
+						emitCss: prod,
+						hotReload: !prod
+					}
+				}
+			},
+			{
+				test: /\.css/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+			},
+            {
+				test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [
+                  {loader: 'file-loader'},
+                  {
+                    loader: 'svgo-loader',
+                    /*options: {
+                      plugins: [
+                        {removeXMLNS: true},
+                        {removeOffCanvasPaths: true},
+                        {removeDimensions: true},
+                        {reusePaths: true}
+                      ]
+                    }*/
+                  }
+				]
+			},
+			{
+                test: /\.(ttf|eot|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader',
+                }]
+            }
+		]
+    },
+	resolve: {
+		alias: {
+			path: "path-browserify",
+		},
+		modules: [
+			'node_modules',
+			'front/javascripts',
+			'libs'
+		],
+	}
+}
