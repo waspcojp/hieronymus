7cb4e61d75c453c1957020721a853bc77add55da
Masami Ogoshi <ogochan@wasp.co.jp>
2023-05-22 03:32:21 +0000

    inventory

--- migrations/20230513065313-change-user.js
+++ migrations/20230513065313-change-user.js
@@ -9,12 +9,20 @@ module.exports = {
     await queryInterface.addColumn('Users', 'administrable', {
       type: Sequelize.BOOLEAN
     });
-    await queryInterface.sequelize.query('UPDATE "Users" set administrable = true, approvable = true where id = 1');
+    await queryInterface.addColumn('Users', 'approvable', {
+      type: Sequelize.BOOLEAN
+    });
+    await queryInterface.addColumn('Users', 'inventory', {
+      type: Sequelize.BOOLEAN
+    });
+    await queryInterface.sequelize.query(
+      'UPDATE "Users" set administrable = true, approvable = true where id = 1, inventory = true');
   },
 
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn('Users', 'approvable');
     await queryInterface.removeColumn('Users', 'administratable');
+    await queryInterface.removeColumn('Users', 'inventory');
     /**
      * Add reverting commands here.
      *
