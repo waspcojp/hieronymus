e6ebbfbfa7669bacd43ca1920ef7da8481289eb5
Masami Ogoshi <ogochan@wasp.co.jp>
2023-06-01 07:16:06 +0000

    user maintennce

---
+++ front/svelte/users/user-list.svelte
@@ -0,0 +1,64 @@
+<div class="row full-height fontsize-12pt" style="overflow-y: scroll;">
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th scope="col">
+					名前
+				</th>
+				<th scope="col">
+					管理者
+				</th>
+				<th scope="col">
+					承認可能
+				</th>
+				<th scope="col">
+					在庫管理
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#await get then result}
+			{#each result.data as line}
+			<tr class="fontsize-12pt">
+				<td>
+					<a href="#" data-no={line.id}>
+						{line.name}
+					</a>
+				</td>
+				<td>
+					{line.administrable}
+				</td>
+				<td>
+					{line.approvable}
+				</td>
+				<td>
+					{line.inventory}
+				</td>
+			</tr>
+			{/each}
+			{/await}
+		</tbody>
+	</table>
+</div>
+
+<style>
+th {
+	text-align: center;
+}
+</style>
+
+<script>
+import axios from 'axios';
+import {onMount, beforeUpdate, afterUpdate, createEventDispatcher} from 'svelte';
+
+export	let	update;
+
+let	get;
+
+onMount(() => {
+
+});
+beforeUpdate(() => {
+	get = axios.get(`/api/users/`);
+});
+</script>
---
+++ front/svelte/users/users.svelte
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">ユーザ一覧</a>
+		<ul class="navbar-nav me-auto mb-2">
+			<li class="nav-item">
+			</li>
+		</ul>
+		<ul class="navbar-nav ms-auto">
+			<li class="nav-item">
+			</li>
+		</ul>
+	</div> 
+</nav>
+<div class="row body-height">
+	<UserList
+			update={list_update}></UserList>
+</div>
+
+<style>
+</style>
+
+<script>
+import {onMount, beforeUpdate, afterUpdate, createEventDispatcher} from 'svelte';
+import UserList from './user-list.svelte';
+
+let	user;
+let	list_update;
+
+const	update = (event) => {
+	console.log('update');
+	list_update = true;
+}
+
+beforeUpdate(()	=> {
+});
+afterUpdate(() => {
+})
+</script>
