2431479e8b64b95ca27428f8919a78e60d491a23
Masami Ogoshi <ogochan@wasp.co.jp>
2023-06-05 11:56:02 +0000

    revoke authenticate

--- migrations/20230513065313-change-user.js
+++ migrations/20230513065313-change-user.js
@@ -3,6 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'deauthorizedAt', {
+      type: Sequelize.DATE
+    });
+    await queryInterface.addColumn('Users', 'accounting', {
+      type: Sequelize.BOOLEAN
+    });
+    await queryInterface.addColumn('Users', 'fiscal_browsing', {
+      type: Sequelize.BOOLEAN
+    });
     await queryInterface.addColumn('Users', 'approvable', {
       type: Sequelize.BOOLEAN
     });
@@ -13,10 +22,13 @@ module.exports = {
       type: Sequelize.BOOLEAN
     });
     await queryInterface.sequelize.query(
-      'UPDATE "Users" set administrable = true, approvable = true, inventory = true where id = 1');
+      'UPDATE "Users" set accounting = true, fiscal_browsing = true, administrable = true, approvable = true, inventory = true where id = 1');
   },
 
   async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'deauthorizedAt');
+    await queryInterface.removeColumn('Users', 'accounting');
+    await queryInterface.removeColumn('Users', 'fiscal_browsing');
     await queryInterface.removeColumn('Users', 'approvable');
     await queryInterface.removeColumn('Users', 'administrable');
     await queryInterface.removeColumn('Users', 'inventory');
