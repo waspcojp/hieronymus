01e3d7cb8dd8afc342667d5b6ea83c0fe967a140
Masami Ogoshi <ogochan@wasp.co.jp>
2023-06-05 11:57:51 +0000

    prepare SPA

---
+++ front/svelte/components/alert.svelte
@@ -0,0 +1,13 @@
+<div class="row">
+    {#if alert}
+    <div class="col-12 alert fade show {alert_level}" role="alert">
+        <button type="button" class="btn-close" aria-label="Close"
+            on:click="{() => {alert = undefined}}"></button>
+        <strong>{alert}</strong>
+    </div>
+    {/if}
+</div>
+<script>
+export let alert;
+export let alert_level;
+</script>
---
+++ front/svelte/home/password.svelte
@@ -0,0 +1,73 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">パスワード変更</h3>
+    </div>
+    <div class="card-body">
+        <form>
+            <div class="row mb-3">
+                <label for="inputCurrentPassword" class="col-sm-5 col-form-label">
+                    現在のパスワード
+                </label>
+                <div class="col-sm-7">
+                    <input type="text" class="form-control" id="inputCurrentPassword"
+                        bind:value={currentPassword}>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="inputNewPassword" class="col-sm-5 col-form-label">
+                    新しいパスワード
+                </label>
+                <div class="col-sm-7">
+                    <input type="text" class="form-control" id="inputNewPassword"
+                        bind:value={newPassword}>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="inputConfirmPassword" class="col-sm-5 col-form-label">
+                    新しいパスワードの再入力
+                </label>
+                <div class="col-sm-7">
+                    <input type="text" class="form-control" id="inputConfirmPassword"
+                        bind:value={confirmPassword}>
+                </div>
+            </div>
+            <button class="btn btn-primary" on:click|preventDefault={updatePassword}>
+                パスワード更新
+            </button>
+        </form>
+    </div>
+</div>
+<script>
+import axios from 'axios';
+
+let currentPassword;
+let newPassword;
+let confirmPassword;
+
+export let alert;
+export let alert_level;
+
+const updatePassword = (event) => {
+    if  ( newPassword ) {
+        if  ( newPassword == confirmPassword )    {
+            axios.put('/api/user/password', {
+                currentPassword: currentPassword,
+                newPassword: newPassword
+            }).then((res) => {
+                alert = 'password update success';
+                alert_level = 'alert-success';
+                currentPassword = '';
+                newPassword = '';
+                confirmPassword = '';
+            }).catch ((e) => {
+                alert = 'password update fail';
+                alert_level = 'alert-danger';
+            });
+        } else {
+            alert = 'invalid password';
+            alert_level = 'alert-danger';
+        }
+    }
+}
+
+</script>
