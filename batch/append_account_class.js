2d3b6f48aa4624dd1c6530ae3c18d57bfc7256c6
Masami Ogoshi <ogochan@wasp.co.jp>
2024-09-04 15:29:00 +0900

    batch modules seperate from libs/

---
+++ batch/append_account_class.js
@@ -0,0 +1,20 @@
+import models from '../models';
+
+export const append_account_class = async (args) => {
+	account_rec = await models.AccountClass.create({
+		major: args.major,
+		middle: args.middle,
+		minor: args.minor,
+		field: args.field,
+		adding: args.adding
+	});
+}
+
+export append_account_class({
+	major: '経常損益',
+	middle: '営業外費用',
+	minor: '法人税住民税等',
+	field: 9,
+	adding: 3
+});
+
